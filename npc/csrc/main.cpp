59b7374ede65d13c21e151f4ffac02c49666a2e5
tracer-ysyx <tracer@ysyx.org>
2024-09-16 16:37:07 +0800

    > sim RTL ysyx_22040000 卢荣昌 Linux WUJIE16-Pro 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  16:37:07 up  3:58,  2 users,  load average: 2.24, 1.80, 1.70

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,8 @@
 #include "Vtop.h"          // Generated from Verilator
 #include "verilated.h"     // Verilator header
 #include "verilated_vcd_c.h" // For VCD waveform tracing
+// For rand()
+#include <cstdlib>
 
 int main(int argc, char **argv) {
     Verilated::commandArgs(argc, argv);  // Pass arguments to Verilator
@@ -18,25 +20,14 @@ int main(int argc, char **argv) {
     top->eval(); // Evaluate the initial state
     tfp->dump(0); // Dump time 0
 
-    // Cycle 1: Apply inputs
-    top->a = 0; top->b = 1;
-    top->eval();
-    tfp->dump(1); // Dump time 1
-
-    // Cycle 2: Apply inputs
-    top->a = 1; top->b = 0;
-    top->eval();
-    tfp->dump(2); // Dump time 2
-
-    // Cycle 3: Apply inputs
-    top->a = 1; top->b = 1;
-    top->eval();
-    tfp->dump(3); // Dump time 3
-
-    // Cycle 4: Apply inputs
-    top->a = 0; top->b = 0;
-    top->eval();
-    tfp->dump(4); // Dump time 4
+    for (int i = 1; i < 100; i++) {
+        // Cycle i: Apply inputs
+        top->a = rand() & 1; top->b = rand() & 1;
+        top->eval();
+        tfp->dump(i); // Dump time 1
+        printf("a = %d, b = %d, f = %d\n", top->a, top->b, top->f);
+        assert(top->f == (top->a ^ top->b));
+    }   
 
     // Close trace and cleanup
     tfp->close();
