5aec9ee76817617fb8079d170be2b340319f6cec
tracer-ysyx <tracer@ysyx.org>
2024-09-27 23:33:30 +0800

    > compile NEMU ysyx_22040000 卢荣昌 Linux WUJIE16-Pro 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  23:33:30 up  1:33,  2 users,  load average: 1.89, 1.76, 1.54

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -459,10 +459,17 @@ int error = false;
 //   return lval;
 // }
 
-void next_token()
+void next_token(int line)
 {
+  char str[44];
+  get_token_str(parse_index, str);
+  Log("NEXT_TOKEN %d: parse_index = %d, type_str = %s, str = %s", line, parse_index, str, tokens[parse_index].str);
   parse_index++;
+  get_token_str(parse_index, str);
+  Log("NEXT_TOKEN %d: parse_index = %d, type_str = %s, str = %s", line, parse_index, str, tokens[parse_index].str);
 }
+
+#define next_token() next_token(__LINE__)
 
 word_t parse_hex(char *str)
 {
