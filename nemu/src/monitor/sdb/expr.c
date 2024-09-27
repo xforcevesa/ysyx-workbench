54f0cc5589f3623ab01c6644e57f42c969768ce7
tracer-ysyx <tracer@ysyx.org>
2024-09-27 20:10:38 +0800

    > compile NEMU ysyx_22040000 卢荣昌 Linux WUJIE16-Pro 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  20:10:38 up  1:36,  2 users,  load average: 1.59, 1.66, 1.56

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -38,13 +38,13 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
-  {"\\+", TK_ADD},         // plus
+  {"+", TK_ADD},         // plus
   {"==", TK_EQ},        // equal
-  {"\\*", TK_MUL},         // asterisk
-  {"\\/", TK_DIV},           // slash
-  {"\\%", TK_MOD},          // percent
-  {"\\>", TK_GT},          // greater than
-  {"\\<", TK_LT},          // less than
+  {"*", TK_MUL},         // asterisk
+  {"/", TK_DIV},           // slash
+  {"%", TK_MOD},          // percent
+  {">", TK_GT},          // greater than
+  {"<", TK_LT},          // less than
   {">=", TK_GE},         // greater than or equal to
   {"<=", TK_LE},         // less than or equal to
 };
