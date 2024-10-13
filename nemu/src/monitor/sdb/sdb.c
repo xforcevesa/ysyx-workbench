028bde4e11a586ce65cd3e5bcbd34b0a61dd9ab6
tracer-ysyx <tracer@ysyx.org>
2024-10-13 13:07:53 +0800

    > compile NEMU ysyx_22040000 卢荣昌 Linux WUJIE16-Pro 6.11.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 30 Sep 2024 23:49:50 +0000 x86_64 GNU/Linux  13:07:53 up  1:12,  2 users,  load average: 1.39, 1.33, 1.36

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -259,7 +259,7 @@ static unsigned evaluate_expression(const char* expression, bool *success) {
     fclose(file);
 
     // Compile the generated C code using gcc
-    if (system("gcc -o temp_eval temp_eval.c") != 0) {
+    if (system("gcc -O0 -Wno-integer-overflow -o temp_eval temp_eval.c") != 0) {
         fprintf(stderr, "Compilation failed\n");
         return 0;
     }
